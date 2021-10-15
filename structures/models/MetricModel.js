b74a0a9ae64f2d617988a0d95e4e5b04f46e7027
Yimura <andreas.maerten@scarlet.be>
2021-10-15 14:19:49 +0200

    fix(Model): Unexpected reserved word

--- structures/models/MetricModel.js
+++ structures/models/MetricModel.js
@@ -1,6 +1,6 @@
 import Model from '../schemas/MetricSchema.js'
 
-export const createIfNotExists = () => {
+export const createIfNotExists = async () => {
     const doc = await Model.findOne({ id: 1 }).exec();
     if (doc) return doc;
 
@@ -13,12 +13,12 @@ export const createIfNotExists = () => {
 };
 
 export const increasePlayCount = (type) => {
-    const update = { 
-        $inc: { 
-            'played.tracks': type === 'track' ? 1 : 0, 
-            'played.playlists': type === 'playlist' ? 1 : 0, 
-            'played.albums': type === 'album' ? 1 : 0 
-        } 
+    const update = {
+        $inc: {
+            'played.tracks': type === 'track' ? 1 : 0,
+            'played.playlists': type === 'playlist' ? 1 : 0,
+            'played.albums': type === 'album' ? 1 : 0
+        }
     };
 
     return Model.findOneAndUpdate({ id: 1 }, update).exec();
